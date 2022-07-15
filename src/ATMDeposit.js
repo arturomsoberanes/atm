b9b0d349c0987356a2c9cb5d038703798ff03294
Arturo Soberanes <arturo.soberanes01@gmail.com>
2022-07-14 20:19:37 -0500

    created and modified files for app

---
+++ src/ATMDeposit.js
@@ -0,0 +1,12 @@
+const ATMDeposit = ({ onChange, isDeposit }) => {
+  const choice = ["Deposit", "Cash Back"];
+  console.log(`ATM isDeposit: ${isDeposit}`);
+  return (
+    <label className="label huge">
+      <h3> {choice[Number(!isDeposit)]}</h3>
+      <input type="number" width="200" onChange={onChange}></input>
+      <input type="submit" width="200" value="Submit"></input>
+    </label>
+  );
+};
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import ATMDeposit from '.ATMDeposit';
 import './App.css';
 
-function App() {
+function App(){
+  let deposit = 0; // state of this transaction
+  const [totalState, setTotalState] = React.useState(0);
+  const [isDeposit, setIsDeposit] = React.useState(true);
+
+  let status = `Account Balance $ ${totalState} `;
+  console.log(`Account Rendered with isDeposit: ${isDeposit}`);
+  const handleChange = event => {
+    console.log(`handleChange ${event.target.value}`);
+    deposit = Number(event.target.value);
+  };
+  const handleSubmit = () => {
+    let newTotal = isDeposit ? totalState + deposit : totalState - deposit;
+    setTotalState(newTotal);
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h2 id="total">{status}</h2>
+      <button onClick={() => setIsDeposit(true)}>Deposit</button>
+      <button onClick={() => setIsDeposit(false)}>Cash Back</button>
+      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}></ATMDeposit>
+    </form>
   );
-}
+};
 
 export default App;
