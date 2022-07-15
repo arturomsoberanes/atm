f66fc274212544b8777ccd5cc463b61035a47869
Arturo Soberanes <arturo.soberanes01@gmail.com>
2022-07-15 11:55:19 -0500

    Improvements in App

--- src/ATMDeposit.js
+++ src/ATMDeposit.js
@@ -1,11 +1,15 @@
-function ATMDeposit({ onChange, isDeposit }){
+function ATMDeposit({ onChange, isDeposit, isValid}){
   const choice = ["Deposit", "Cash Back"];
   console.log(`ATM isDeposit: ${isDeposit}`);
   return (
-    <label className="label huge">
+    <label>
       <h3> {choice[Number(!isDeposit)]}</h3>
-      <input type="number" width="200" onChange={onChange}></input>
-      <input type="submit" width="200" value="Submit"></input>
+      <input id="txtForm" type="number" width="200" onChange={onChange}></input>
+      {isValid === false && <p>You don´t have money</p>}
+      {isValid
+        ? <input type="submit" width="200" value={choice[Number(!isDeposit)]} className="enable"></input>
+        : <input type="submit" width="200" value={choice[Number(!isDeposit)]} disabled className="disable"></input>
+      }
     </label>
   );
 };
--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import atmImg from './img/ATM.svg';
 import React from 'react'
 import ATMDeposit from './ATMDeposit';
 import './App.css';
 
 function App(){
   let deposit = 0; // state of this transaction
+  const [atmMode, setAtmMode] = React.useState("");
+  const [isValid, setIsValid] = React.useState(false);
   const [totalState, setTotalState] = React.useState(0);
   const [isDeposit, setIsDeposit] = React.useState(true);
 
@@ -13,20 +16,48 @@ function App(){
   const handleChange = e => {
     console.log(`handleChange ${e.target.value}`);
     deposit = Number(e.target.value);
+    if(atmMode === "Cash Back" && deposit > totalState){
+      setIsValid(false);
+    }else{
+      setIsValid(true);
+    }
   };
   const handleSubmit = (e) => {
     let newTotal = isDeposit ? totalState + deposit : totalState - deposit;
     setTotalState(newTotal);
+    e.target.txtForm.value = "";
     e.preventDefault();
   };
+  const selectChange = (e) => {
+    let mode = e.target.value;
+    if (mode === "") setAtmMode("");
+    if (mode === "Deposit"){
+      setAtmMode("Deposit");
+      setIsDeposit(true);
+      setIsValid(true);
+    }
+    if (mode === "Cash Back"){
+      setAtmMode("Cash Back");
+      setIsDeposit(false);
+    }
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2 id="total">{status}</h2>
-      <button onClick={() => setIsDeposit(true)}>Deposit</button>
-      <button onClick={() => setIsDeposit(false)}>Cash Back</button>
-      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}></ATMDeposit>
-    </form>
+    <div className="form-container">
+      <img src={atmImg} alt="ATM image" />
+      <form onSubmit={handleSubmit}>
+        <h2 id="total">{status}</h2>
+        <select id="ATMoptions" onChange={selectChange}>
+          <option value=""></option>
+          <option value="Deposit">Deposit</option>
+          <option value="Cash Back">Cash Back</option>
+        </select>
+        {atmMode === ""
+          ? <p>Please choose</p>
+          : <ATMDeposit onChange={handleChange} isDeposit={isDeposit} isValid={isValid}></ATMDeposit>
+        }
+      </form>
+    </div>
   );
 };
 
